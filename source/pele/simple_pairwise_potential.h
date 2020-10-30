6b0b268776b7f684bff24bbc1a6c37a2bcfbedfa
Praharsh Suryadevara <praharsharmm@gmail.com>
2020-10-30 12:34:06 -0500

    simple pairwise add_energy_gradient_hessian_sparse initial

--- source/pele/simple_pairwise_potential.h
+++ source/pele/simple_pairwise_potential.h
@@ -6,12 +6,12 @@
 
 #include "pairwise_potential_interface.h"
 #include "array.h"
+#include "petscsystypes.h"
+#include "petscvec.h"
 #include "vecn.h"
 #include "distance.h"
 #include <omp.h>
 #include <petscmat.h>
-
-
 
 extern "C" {
 #include "xsum.h"
@@ -87,6 +87,7 @@ public:
         };
         return add_energy_gradient(x, exact_grad);
     }
+
     virtual double get_energy_gradient_hessian(Array<double> const & x, Array<double> & grad, Array<double> & hess)
     {
         grad.assign(0);
@@ -96,8 +97,9 @@ public:
     virtual double add_energy_gradient(Array<double> const & x, Array<double> & grad);
     virtual double add_energy_gradient(Array<double> const & x, std::vector<xsum_small_accumulator> & exact_grad);
     virtual double add_energy_gradient_hessian(Array<double> const & x, Array<double> & grad, Array<double> & hess);
+    virtual double add_energy_gradient_hessian_sparse(Array<double> const & x, Vec & grad, Mat & hess);
     virtual void get_neighbors(pele::Array<double> const & coords,
-                                pele::Array<std::vector<size_t>> & neighbor_indss,
+                               pele::Array<std::vector<size_t>> & neighbor_indss,
                                 pele::Array<std::vector<std::vector<double>>> & neighbor_distss,
                                 const double cutoff_factor = 1.0);
     virtual void get_neighbors_picky(pele::Array<double> const & coords,
@@ -223,12 +225,12 @@ template<typename pairwise_interaction, typename distance_policy>
         double gij;
         double dr[m_ndim];
         // std::vector<double> grad_test (grad.size(), 0);
-
-    
+        
+        
         if (!exact_gradient_initialized) {
-            exact_gradient = std::make_shared<std::vector<xsum_small_accumulator>>(exact_grad.size());
-            exact_gradient_initialized=true;
-        }
+                exact_gradient = std::make_shared<std::vector<xsum_small_accumulator>>(exact_grad.size());
+                exact_gradient_initialized=true;
+            }
 
         for (size_t i=0; i < exact_grad.size(); ++i) {
             xsum_small_init(&((*exact_gradient)[i]));
@@ -347,53 +349,166 @@ inline double SimplePairwisePotential<pairwise_interaction, distance_policy>::ad
     return e;
 }
 
+
+/**
+ * Finds a sparse hessian
+ */
 template<typename pairwise_interaction, typename distance_policy>
-inline double SimplePairwisePotential<pairwise_interaction, distance_policy>::get_energy(Array<double> const & x)
-{
+inline double SimplePairwisePotential<pairwise_interaction, distance_policy>::add_energy_gradient_hessian_sparse(Array<double> const & x, Vec & grad, Mat & hess) {
+    double hij, gij;
+    double dr[m_ndim];
+    const size_t N = x.size();
     const size_t natoms = x.size() / m_ndim;
     if (m_ndim * natoms != x.size()) {
         throw std::runtime_error("x.size() is not divisible by the number of dimensions");
     }
-    double e=0.;
-    double dr[m_ndim];
+    // if (x.size() != grad.size()) {
+    //     throw std::invalid_argument("the gradient has the wrong size");
+    // }
+    // if (hess.size() != x.size() * x.size()) {
+    //     throw std::invalid_argument("the Hessian has the wrong size");
+    // }
 
+    double e = 0.;
+    double gradi[m_ndim];             // gradi
+    double gradj[m_ndim];             // gradj
+    int indicesi[3];    // indices upto 3
+    int indicesj[3];    // indices upto 3
     for (size_t atom_i=0; atom_i<natoms; ++atom_i) {
         size_t i1 = m_ndim * atom_i;
-        for (size_t atom_j=0; atom_j<atom_i; ++atom_j) {
+        for (size_t atom_j=0; atom_j<atom_i; ++atom_j){
             size_t j1 = m_ndim * atom_j;
 
             _dist->get_rij(dr, &x[i1], &x[j1]);
             double r2 = 0;
-            #pragma unroll
+#pragma unroll
             for (size_t k=0; k<m_ndim; ++k) {
                 r2 += dr[k]*dr[k];
             }
+            
+            e += _interaction->energy_gradient_hessian(r2, &gij, &hij, sum_radii(atom_i, atom_j));
+            if (gij != 0) {
+#pragma unroll
+for (size_t k=0; k<m_ndim; ++k) {
+                     gradi[k] = -gij * dr[k];
+                     gradj[k] = gij * dr[k];
+                     indicesi[k] = i1+k;
+                     indicesj[k] = j1+k;
+                 }
+                VecSetValues(grad, m_ndim, indicesi, gradi, ADD_VALUES);
+                VecSetValues(grad, m_ndim, indicesj, gradj, ADD_VALUES);
+            }
+            VecAssemblyBegin(grad);
+            VecAssemblyEnd(grad);
 
-            e += _interaction->energy(r2, sum_radii(atom_i, atom_j));
+            // grad[i1+k] -= gij * dr[k];
+            // grad[j1+k] += gij * dr[k];
+            // define diagonal and off diagonal blocks
+            // not setting 0s since too many of these are 0
+            
+            if (hij != 0) {
+#pragma unroll
+                for (size_t k=0; k<m_ndim; ++k){
+                    //diagonal block - diagonal terms
+                    double Hii_diag = (hij+gij)*dr[k]*dr[k]/r2 - gij;
+                    MatSetValue(hess, i1+k, i1+k, Hii_diag, ADD_VALUES);
+                    MatSetValue(hess, j1+k, j1+k, Hii_diag, ADD_VALUES);
+                    // hess[N*(i1+k)+i1+k] += Hii_diag;
+                    // hess[N*(j1+k)+j1+k] += Hii_diag;
+                    //off diagonal block - diagonal terms
+                    MatSetValue(hess, i1+k, j1+k, -Hii_diag, ADD_VALUES);
+#pragma unroll
+                    for (size_t l = k+1; l<m_ndim; ++l){
+                        //diagonal block - off diagonal terms
+                        double Hii_off = (hij+gij)*dr[k]*dr[l]/r2;
+                        if (Hii_off !=0)
+                            {
+                                MatSetValue(hess, i1 +k, i1+l, Hii_off, ADD_VALUES);
+                                MatSetValue(hess, j1+k, j1+l, Hii_off, ADD_VALUES);
+                                //off diagonal block - off diagonal terms
+                                MatSetValue(hess, i1+k, j1+l, -Hii_off, ADD_VALUES);
+                                MatSetValue(hess, j1+k, i1+l, -Hii_off, ADD_VALUES);
+                            }
+                    }
+                    //                     for (size_t k=0; k<m_ndim; ++k){
+                    //                         //diagonal block - diagonal terms
+                    //                         double Hii_diag = (hij+gij)*dr[k]*dr[k]/r2 - gij;
+                    //                         hess[N*(i1+k)+i1+k] += Hii_diag;
+                    //                         hess[N*(j1+k)+j1+k] += Hii_diag;
+                    //                         //off diagonal block - diagonal terms
+                    //                         double Hij_diag = -Hii_diag;
+                    //                         hess[N*(i1+k)+j1+k] = Hij_diag;
+                    //                         hess[N*(j1+k)+i1+k] = Hij_diag;
+                    // #pragma unroll
+                    //                         for (size_t l = k+1; l<m_ndim; ++l){
+                    //                             //diagonal block - off diagonal terms
+                    //                             double Hii_off = (hij+gij)*dr[k]*dr[l]/r2;
+                    //                             hess[N*(i1+k)+i1+l] += Hii_off;
+                    //                             hess[N*(i1+l)+i1+k] += Hii_off;
+                    //                             hess[N*(j1+k)+j1+l] += Hii_off;
+                    //                             hess[N*(j1+l)+j1+k] += Hii_off;
+                    //                             //off diagonal block - off diagonal terms
+                    //                             double Hij_off = -Hii_off;
+                    //                             hess[N*(i1+k)+j1+l] = Hij_off;
+                    //                             hess[N*(i1+l)+j1+k] = Hij_off;
+                    //                             hess[N*(j1+k)+i1+l] = Hij_off;
+                    //                             hess[N*(j1+l)+i1+k] = Hij_off;
+                        
+                    //                         }
+                    
+                }
+            }
         }
     }
-    return e;
 }
 
 template<typename pairwise_interaction, typename distance_policy>
+inline double SimplePairwisePotential<pairwise_interaction, distance_policy>::get_energy(Array<double> const & x)
+    {
+        const size_t natoms = x.size() / m_ndim;
+        if (m_ndim * natoms != x.size()) {
+            throw std::runtime_error("x.size() is not divisible by the number of dimensions");
+        }
+        double e=0.;
+        double dr[m_ndim];
+
+        for (size_t atom_i=0; atom_i<natoms; ++atom_i) {
+            size_t i1 = m_ndim * atom_i;
+            for (size_t atom_j=0; atom_j<atom_i; ++atom_j) {
+                size_t j1 = m_ndim * atom_j;
+
+                _dist->get_rij(dr, &x[i1], &x[j1]);
+                double r2 = 0;
+#pragma unroll
+                for (size_t k=0; k<m_ndim; ++k) {
+                    r2 += dr[k]*dr[k];
+                }
+
+                e += _interaction->energy(r2, sum_radii(atom_i, atom_j));
+            }
+        }
+        return e;
+    }
+
+template<typename pairwise_interaction, typename distance_policy>
 void SimplePairwisePotential<pairwise_interaction, distance_policy>::get_neighbors(
-    pele::Array<double> const & coords,
-    pele::Array<std::vector<size_t>> & neighbor_indss,
-    pele::Array<std::vector<std::vector<double>>> & neighbor_distss,
-    const double cutoff_factor /*=1.0*/)
+                                                                                   pele::Array<double> const & coords,
+                                                                                   pele::Array<std::vector<size_t>> & neighbor_indss,
+                                                                                   pele::Array<std::vector<std::vector<double>>> & neighbor_distss,
+                                                                                   const double cutoff_factor /*=1.0*/)
 {
     const size_t natoms = coords.size() / m_ndim;
-    pele::Array<short> include_atoms(natoms, 1);
-    get_neighbors_picky(coords, neighbor_indss, neighbor_distss, include_atoms, cutoff_factor);
+        pele::Array<short> include_atoms(natoms, 1);
+        get_neighbors_picky(coords, neighbor_indss, neighbor_distss, include_atoms, cutoff_factor);
 }
 
 template<typename pairwise_interaction, typename distance_policy>
 void SimplePairwisePotential<pairwise_interaction, distance_policy>::get_neighbors_picky(
-    pele::Array<double> const & coords,
-    pele::Array<std::vector<size_t>> & neighbor_indss,
-    pele::Array<std::vector<std::vector<double>>> & neighbor_distss,
-    pele::Array<short> const & include_atoms,
-    const double cutoff_factor /*=1.0*/)
+                                                                                         pele::Array<double> const & coords,
+                                                                                         pele::Array<std::vector<size_t>> & neighbor_indss,
+                                                                                         pele::Array<std::vector<std::vector<double>>> & neighbor_distss,
+                                                                                         pele::Array<short> const & include_atoms,
+                                                                                         const double cutoff_factor /*=1.0*/)
 {
     const size_t natoms = coords.size() / m_ndim;
     if (m_ndim * natoms != coords.size()) {
@@ -422,7 +537,7 @@ void SimplePairwisePotential<pairwise_interaction, distance_policy>::get_neighbo
 
                     _dist->get_rij(dr.data(), &coords[i1], &coords[j1]);
                     double r2 = 0;
-                    #pragma unroll
+#pragma unroll
                     for (size_t k=0; k<m_ndim; ++k) {
                         r2 += dr[k]*dr[k];
                         neg_dr[k] = -dr[k];
@@ -444,8 +559,8 @@ void SimplePairwisePotential<pairwise_interaction, distance_policy>::get_neighbo
 }
 
 template<typename pairwise_interaction, typename distance_policy>
-std::vector<size_t> SimplePairwisePotential<pairwise_interaction, distance_policy>::get_overlaps(
-    Array<double> const & coords)
+        std::vector<size_t> SimplePairwisePotential<pairwise_interaction, distance_policy>::get_overlaps(
+                                                                                                         Array<double> const & coords)
 {
     const size_t natoms = coords.size() / m_ndim;
     if (m_ndim * natoms != coords.size()) {
@@ -465,7 +580,7 @@ std::vector<size_t> SimplePairwisePotential<pairwise_interaction, distance_polic
 
             _dist->get_rij(dr.data(), &coords[i1], &coords[j1]);
             double r2 = 0;
-            #pragma unroll
+#pragma unroll
             for (size_t k=0; k<m_ndim; ++k) {
                 r2 += dr[k]*dr[k];
             }
@@ -479,7 +594,7 @@ std::vector<size_t> SimplePairwisePotential<pairwise_interaction, distance_polic
         }
     }
     return overlap_inds;
-}
+    }
 
 } // namespace pele
 
