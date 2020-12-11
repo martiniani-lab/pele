8a9dccf599624a350b64b36d2cbab6b75e4b0635
Praharsh Suryadevara <praharsharmm@gmail.com>
2020-12-11 16:57:07 -0600

    petsc interface functions

--- source/pele/petsc_ts.hpp
+++ source/pele/petsc_ts.hpp
@@ -1,49 +1,49 @@
-#ifndef _PELE_CVODE_OPT_H__
-#define _PELE_CVODE_OPT_H__
-/**
- * "optimizer" based on PETSc's TS solver. Not technically an optimizer, but
- * should help us identify the path of steepest descent.
- */
+// #ifndef _PELE_CVODE_OPT_H__
+// #define _PELE_CVODE_OPT_H__
+// /**
+//  * "optimizer" based on PETSc's TS solver. Not technically an optimizer, but
+//  * should help us identify the path of steepest descent.
+//  */
 
-#include "base_potential.hpp"
-#include "array.hpp"
-#include "debug.hpp"
-#include "optimizer.hpp"
+// #include "base_potential.hpp"
+// #include "array.hpp"
+// #include "debug.hpp"
+// #include "optimizer.hpp"
 
-#include "petscsnes.h"
-#include "petscsys.h"
-#include "petscsystypes.h"
-#include "petscvec.h"
-#include <petscksp.h>
-
-
-namespace pele {
+// #include "petscsnes.h"
+// #include "petscsys.h"
+// #include "petscsystypes.h"
+// #include "petscvec.h"
+// #include <petscksp.h>
 
 
-class PETScTSOptimizer : public GradientOptimizer {
-private:
-    size_t N_size;
-    double t0;
-    double tN;
-    // sparse calculation initializers
-    Mat petsc_jacobian;
-    Vec petsc_grad;
-    Vec residual;
-    PetscInt blocksize;
-    // average number of non zeros per block for memory allocation purposes
-    PetscInt hessav;
-    SNES  snes;
-public:
-    void one_iteration();
-    PETScTSOptimizer(std::shared_ptr<pele::BasePotential> potential,
-                     const pele::Array<double> x0,
-                     double tol=1e-5,
-                     double rtol=1e-4,
-                     double atol=1e-4);
-    ~PETScTSOptimizer();
+// namespace pele {
+
+
+// class PETScTSOptimizer : public GradientOptimizer {
+// private:
+//     size_t N_size;
+//     double t0;
+//     double tN;
+//     // sparse calculation initializers
+//     Mat petsc_jacobian;
+//     Vec petsc_grad;
+//     Vec residual;
+//     PetscInt blocksize;
+//     // average number of non zeros per block for memory allocation purposes
+//     PetscInt hessav;
+//     SNES  snes;
+// public:
+//     void one_iteration();
+//     PETScTSOptimizer(std::shared_ptr<pele::BasePotential> potential,
+//                      const pele::Array<double> x0,
+//                      double tol=1e-5,
+//                      double rtol=1e-4,
+//                      double atol=1e-4);
+//     ~PETScTSOptimizer();
     
-};
-}
+// };
+// }
 
 
 
@@ -51,4 +51,4 @@ public:
 
 
 
-#endif
+// #endif
