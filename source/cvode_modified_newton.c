76f4c1b860cfbbc4bce0f39564b0160c534ee9eb
Praharsh Suryadevara <praharsharmm@gmail.com>
2020-12-17 20:02:41 -0600

    modified newton setup wrapper around SNES

--- source/cvode_modified_newton.c
+++ source/cvode_modified_newton.c
@@ -10,10 +10,15 @@
 #include "pele/cvode_modified_newton.h"
 #include "cvode/cvode.h"
 #include <petscerror.h>
+#include <petscksp.h>
 #include <petscmat.h>
+#include <petscpc.h>
+#include <petscpctypes.h>
 #include <petscsnes.h>
+#include <petscsys.h>
 #include <petscsystypes.h>
 #include <petscvec.h>
+#include <string.h>
 #include <sundials/sundials_types.h>
 
 /* private macros */
@@ -113,7 +118,7 @@ PetscErrorCode CVDelayedJSNES(SNES snes, Vec X, Mat A, Mat Jpre,
  TODO: THis is not necessary for us since we're planning to go with a
  solver. but iterative solvers need this set up
   -----------------------------------------------------------------*/
-PetscErrorCode cvLSPresolveKSP(KSP ksp, Vec b, Vec x, void *ctx) { return 0; }
+PetscErrorCode cvLSPreSolveKSP(KSP ksp, Vec b, Vec x, void *ctx) { return 0; }
 
 /*-----------------------------------------------------------------
   Calls the post solve routine for the KSP Solver to emulate the
@@ -139,8 +144,8 @@ PetscErrorCode cvLSPostSolveKSP(KSP ksp, Vec b, Vec x, void *context) {
 /* allocates extra memory for running the modified newton algorithm using
  * PETSc*/
 CVMNPETScMem CVODEMNPETScCreate(void *cvode_mem, void *user_mem,
-                                       CVSNESJacFn func, booleantype scalesol,
-                                       Mat Jac, Vec y) {
+                                CVSNESJacFn func, booleantype scalesol, Mat Jac,
+                                Vec y) {
   CVMNPETScMem content;
 
   /* allocate memory for content */
@@ -170,19 +175,20 @@ CVMNPETScMem CVODEMNPETScCreate(void *cvode_mem, void *user_mem,
   return content;
 }
 
-
-
 /* destructor for cvmnpetscmem */
 PetscErrorCode CVODEMNPTScFree(CVMNPETScMem *cvmnpetscmem) {
   if (cvmnpetscmem == NULL) {
-      return 0;
+    return 0;
   }
   PetscErrorCode ierr;
 
   /* destroy vectors/matrices */
-  ierr = MatDestroy(&((*cvmnpetscmem)->savedJ));CHKERRQ(ierr);
-  ierr = VecDestroy(&((*cvmnpetscmem)->fcur));CHKERRQ(ierr);
-  ierr = VecDestroy(&((*cvmnpetscmem)->ycur));CHKERRQ(ierr);
+  ierr = MatDestroy(&((*cvmnpetscmem)->savedJ));
+  CHKERRQ(ierr);
+  ierr = VecDestroy(&((*cvmnpetscmem)->fcur));
+  CHKERRQ(ierr);
+  ierr = VecDestroy(&((*cvmnpetscmem)->ycur));
+  CHKERRQ(ierr);
 
   /* Free matrix vectors */
   free(cvmnpetscmem);
@@ -190,5 +196,67 @@ PetscErrorCode CVODEMNPTScFree(CVMNPETScMem *cvmnpetscmem) {
   return 0;
 }
 
+/* -----------------------------------------------------------------------------
+   Sets up SNES to use the delayed newton approach with SUNNonlinSol
+   in SUNDIALS to save Jacobian evaluations for small systems.
 
 
+   This routine has the following functions
+   1. Sets the krylov solver to use the preconditioner only
+      which we want to do if we want to use PCLU and PCCholesky
+      as direct solvers.
+   2. Sets Newton to use no line search. this is important
+      considering that our step size modification is integrated
+      into the solver itself.
+   3. sets up the pre solve and postsolve routines to scale
+      the solution appropriately according to stepsize.
+
+
+   Developer's Note: This hasn't been setup for iterative methods.
+
+   Warning: In sundials language the Jacobian is the gradient of
+   the RHS of the ODE,
+
+   dx/dt = f(x, t)
+
+   to be solved. This is the language we're
+   using here. For SNES however the Jacobian is the gradient of
+   the LHS of the Nonlinear system
+
+   F(x)=b
+
+   This difference in language is important to keep in mind. since
+   this function is called by SNESSetJacobian which refers the
+   Jacobian in SNES language.
+   Both of these are related by A = J_{SNES} = I-\gamma J_{cvode}
+
+   TODO: maybe remove the wrapper around the setup by defining a
+   wrapped around free function
+ * ---------------------------------------------------------------------------*/
+PetscErrorCode CVSNESMNSetup(SNES snes, CVMNPETScMem cvmnmem) {
+    PetscFunctionBegin;
+    KSP ksp;
+    PC pc;
+    PCType pc_type;
+    /* get all necessary contexts */
+    SNESGetKSP(snes, &ksp);
+    KSPGetPC(ksp, &pc);
+    PCGetType(pc, &pc_type);    
+
+    
+    /* force the solver to be just use the preconditioner only */
+    KSPSetType(ksp, KSPPREONLY);
+
+    /* checks that the preconditioner is only LU or CHOLESKY */
+
+
+    /* If the Preconditioner is not LU or Cholesky, return no support exit code */
+    if (!(strcmp(pc_type, PCLU) || strcmp(pc_type, PCCHOLESKY))) {
+        PetscFunctionReturn(PETSC_ERR_SUP);
+    }
+
+    /* set presolve and postsolve */
+    KSPSetPreSolve(ksp, cvLSPreSolveKSP, (void *) cvmnmem);
+    KSPSetPostSolve(ksp, cvLSPostSolveKSP, (void *)cvmnmem);
+    PetscFunctionReturn(0);
+}
