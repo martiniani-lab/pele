71a982ba8d7c5d9a962619322789ced9ca7a6e1d
Praharsh Suryadevara <praharsharmm@gmail.com>
2021-11-02 19:04:41 -0500

    bugs + formatting: Newton

--- source/newton.cpp
+++ source/newton.cpp
@@ -2,6 +2,8 @@
 #include "pele/array.hpp"
 #include "pele/optimizer.hpp"
 
+using pele::Array;
+
 namespace pele {
 
 Newton::Newton(std::shared_ptr<BasePotential> potential,
@@ -28,22 +30,30 @@ void Newton::one_iteration() {
   Array<double> x_pele(_x.data(), _x.size());
 
   // compute gradient and hessian
-  _energy = potential_->get_gradient(_x, gradient_pele, hessian_pele);
+  _energy = potential_->get_energy_gradient_hessian(x_pele, gradient_pele,
+                                                    hessian_pele);
   _nhev += 1;
 
   // calculate the newton step
-  x_ = _hessian.completeOrthogonalDecomposition().solve(_gradient);
+  _x = _hessian.completeOrthogonalDecomposition().solve(_gradient);
 
-  _step = x_ - _x_old;
+  _step = _x - _x_old;
 
   // use line search to find the step size
   Array<double> gradient_old_pele(_gradient_old.data(), _gradient_old.size());
   Array<double> x_old_pele(_x_old.data(), _x_old.size());
 
-  _line_search.set_xold_gold(_x_old, gradient_old_pele);
-  _line_search.set_g_f_ptr(g_);
+  _line_search.set_xold_gold_(x_old_pele, gradient_old_pele);
+  _line_search.set_g_f_ptr(gradient_pele);
 
-  double stepnorm = _line_search.line_search(x_, _step);
+  Array<double> step_pele(_step.data(), _step.size());
+
+  double stepnorm = _line_search.line_search(x_pele, step_pele);
+
+  // Hacky since we're not wrapping the original pele arrays. but we can change
+  // this if this if it becomes a speed constraint/causes maintainability issues
+  x_ = x_pele;
+  g_ = gradient_pele;
 }
 
 } // namespace pele
