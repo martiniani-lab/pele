ed378babbf986018e7c27bbe5f8b010ce42ee58a
Praharsh Suryadevara <praharsharmm@gmail.com>
2021-11-06 17:20:53 -0500

    end only mixed descent

---
+++ source/mxd_end_only.cpp
@@ -0,0 +1,92 @@
+/**
+ * @file mxd_end_only.cpp
+ * @author Praharsh Suryadevara
+ * @brief
+ * @version 0.1
+ * @date 2021-11-05
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "pele/mxd_end_only.hpp"
+#include "pele/array.hpp"
+#include "pele/newton.hpp"
+#include "pele/optimizer.hpp"
+#include <Eigen/Dense>
+#include <iostream>
+
+namespace pele {
+
+MixedDescentEndOnly::MixedDescentEndOnly(
+    std::shared_ptr<BasePotential> potential, const pele::Array<double> x0,
+    double tol, double newton_step_tol, double rtol, double atol,
+    double threshold, bool iterative)
+    : GradientOptimizer(potential, x0, tol),
+      _cvode_optimizer(potential, x0, tol, rtol, atol,
+                        iterative, false), // initialize the CVODE optimizer
+      _newton_optimizer(potential, x0, tol,
+                        threshold), // initialize the Newton optimizer
+      _tol(tol), _newton_step_tol(newton_step_tol), use_newton_step(false),
+      particle_disp(potential->get_ndim()) {}
+
+void MixedDescentEndOnly::one_iteration() {
+  if (use_newton_step) {
+    _newton_optimizer.one_iteration();
+  } else {
+    _cvode_optimizer.one_iteration();
+    Array<double> gradient = _cvode_optimizer.get_g();
+    // use standard stopping criteria to figure out when to refine the step
+    rms_ = norm(gradient) / sqrt(gradient.size());
+
+    std::cout << "rms: " << rms_ << std::endl;
+    if (rms_ < _tol) {
+      use_newton_step = true;
+      _newton_optimizer.set_x(_cvode_optimizer.get_x());
+    }
+  }
+  iter_number_++;
+}
+
+
+
+// The stop criteria refines the minima so that the we're closer to the minima.
+bool MixedDescentEndOnly::stop_criterion_satisfied() {
+  // stop criterion can only be satisfied in the Newton regime
+  //std::cout << _newton_optimizer.get_niter() << "niter" << std::endl;
+  if (use_newton_step && _newton_optimizer.get_niter() > 0) {
+    //std::cout << "are we actually here" << std::endl;
+    double _ndim = potential_->get_ndim();
+    int nparticles = _cvode_optimizer.get_x().size() / _ndim;
+    Eigen::VectorXd _step = _newton_optimizer.get_step();
+
+
+    particle_disp.assign(0.0);
+    double max_disp = 0.0;
+
+    for (size_t particle_index = 0; particle_index < nparticles;
+         particle_index++) {
+      for (size_t dim = 0; dim < _ndim; dim++) {
+          particle_disp[dim] =
+              _step(particle_index * _ndim + dim);
+      }
+    double disp = norm(particle_disp);
+      if (disp > max_disp) {
+        max_disp = disp;
+      }
+    }
+    if (max_disp < _newton_step_tol) {
+        //std::cout << "max_disp: " << max_disp << std::endl;
+      return true;
+    }
+    else {
+      return false;
+    }
+
+  } else {
+    return false;
+  }
+  //std::cout << "should not be here" << std::endl;
+}
+
+} // namespace pele
---
+++ source/pele/mxd_end_only.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file mxd_end_only.hpp
+ * @author Praharsh Suryadevara (praharsharmm@gmail.com)
+ * @brief Mixed descent that switches to newton only at the end of the optimization.
+ * @version 0.1
+ * @date 2021-11-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#ifndef _PELE_MXD_END_ONLY_HPP
+#define _PELE_MXD_END_ONLY_HPP
+
+#include "base_potential.hpp"
+#include "pele/array.hpp"
+#include "pele/cvode.hpp"
+#include "pele/newton.hpp"
+#include <complex>
+#include <iostream>
+
+
+
+
+
+namespace pele {
+
+    class MixedDescentEndOnly : public GradientOptimizer {
+        private:
+            double _tol;
+            double _newton_step_tol;
+            bool use_newton_step;
+            Array<double> particle_disp; // Stores displacement for a single particle
+            double _max_dist; // Maximum distance a single particle has moved
+
+            CVODEBDFOptimizer _cvode_optimizer;
+            Newton _newton_optimizer;
+        public:
+        MixedDescentEndOnly(std::shared_ptr<BasePotential> potential, const pele::Array<double> x0, double tol=1e-6, double newton_step_tol=1e-6, double rtol=1e-6, double atol=1e-6,double threshold=1e-16, bool iterative=false);
+        ~MixedDescentEndOnly() {};
+
+        void one_iteration();
+        // since most of the calculations are done in the attached optimizers, we defer the calculations to them
+        inline int get_nhev() { 
+            std::cout << "cvode nhev" << _cvode_optimizer.get_nhev() << std::endl;
+            std::cout << "newton nhev" << _newton_optimizer.get_nhev() << std::endl;
+            return _cvode_optimizer.get_nhev() + _newton_optimizer.get_nhev(); }
+        inline int get_niter() { return iter_number_;}
+        inline double get_nfev() { return _cvode_optimizer.get_nfev() + _newton_optimizer.get_nfev(); }
+        bool stop_criterion_satisfied();
+
+
+};
+}
+
+
+#endif // _PELE_MXD_END_ONLY_HPP
+
+
+
