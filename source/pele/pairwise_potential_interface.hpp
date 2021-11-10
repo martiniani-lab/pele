fc7883e531ab6385915b6aec8b2847dcde6b8194
Praharsh Suryadevara <praharsharmm@gmail.com>
2021-11-10 15:47:06 -0600

    find rattlers c++ implementation

--- source/pele/pairwise_potential_interface.hpp
+++ source/pele/pairwise_potential_interface.hpp
@@ -14,7 +14,10 @@ namespace pele {
 class PairwisePotentialInterface : public BasePotential {
 protected:
   const Array<double> m_radii;
-  Array<bool> m_rattlers; // true if the atom is a rattler
+
+  // Data members for finding rattlers close to minima
+  Array<bool> m_not_rattlers; // true if the atom is a rattler
+  bool m_jammed;              // true if the system is jammed
 
   double sum_radii(const size_t atom_i, const size_t atom_j) const {
     if (m_radii.size() == 0) {
@@ -25,12 +28,13 @@ protected:
   }
 
 public:
-  PairwisePotentialInterface() : m_radii(0) {}
-
+  PairwisePotentialInterface() : m_radii(0) {
+    // rattler finding at minima not implemented when radii are not explicitly
+    // set
+  }
   PairwisePotentialInterface(pele::Array<double> const &radii)
-      : m_radii(radii.copy()), m_rattlers(radii.size(), false)
-
-  {
+      : m_radii(radii.copy()), m_not_rattlers(radii.size(), false),
+        m_jammed(false) {
     if (radii.size() == 0) {
       throw std::runtime_error(
           "PairwisePotentialInterface: illegal input: radii");
@@ -128,21 +132,23 @@ public:
     return pele::Array<size_t>(0);
   }
 
-  pele::Array<bool> get_rattlers() const { return m_rattlers.copy(); }
+  pele::Array<bool> get_rattlers() const { return m_not_rattlers.copy(); }
 
   /**
    * @brief Finds the rattlers in the Found minimum and saves them
    * @param coords The coordinates of the found minimum. important: Needs to be
    * at the minimum
+   * To look at a readable implementation, see the julia code in
+   * check_same_structure.jl in the basinerrror library. (It's also probably
+   * faster since this isn't vectorized)
    */
   void find_rattlers(pele::Array<double> const &minimum_coords) {
 
-    m_rattlers.assign(false);
+    m_not_rattlers.assign(true);
     size_t dim = get_ndim();
 
     // Not implemented for higher dimensions
-    assert(dim ==2)
-    size_t n_particles = minimum_coords.size() / dim;
+    assert(dim == 2) size_t n_particles = minimum_coords.size() / dim;
 
     size_t zmin = dim + 1
 
@@ -164,6 +170,10 @@ public:
 
     bool found_rattler = false;
     size_t no_of_neighbors = 0;
+    // allocation for the index of atom i in the neighbors of atom j
+    size_t i_in_j;
+    std::vector<size_t> neighbors_of_atom;
+
     while (rattler_check_list.size() > 0) {
       current_check_list = rattler_check_list;
       rattler_check_list.clear();
@@ -173,10 +183,54 @@ public:
         if (no_of_neighbors < zmin) {
           found_rattler = true;
         } else {
-        found_rattler = origin_in_hull_2d(neighbor_distss[atomi])
+          found_rattler = origin_in_hull_2d(neighbor_distss[atomi]);
+        }
+        if (found_rattler) {
+          m_not_rattlers[atomi] = false;
+          n_rattlers++;
+
+          // remove atom from the list of atoms to check if present in the check
+          // list
+          if (current_check_list.find(atomi) != current_check_list.end()) {
+            current_check_list.erase(atomi);
+          }
+
+          for (size_t neighbor_index : neighbor_indss[atomi]) {
+            rattler_check_list.insert(neighbor_index);
+            neighbors_of_atom = neighbor_indss[neighbor_index];
+            for (size_t j = 0; j < neighbors_of_atom.size(); ++j) {
+              i_in_j = neighbors_of_atom[j];
+              if (current_check_list.find(i_in_j) != current_check_list.end()) {
+                current_check_list.erase(i_in_j);
+              }
+              // remove the displacements of the rattlers from neigbhor_distss
+              for (size_t k = 0; k < neighbor_distss[i_in_j].size(); ++k) {
+                if (neighbor_distss[i_in_j][k][0] == atomi) {
+                  neighbor_distss[i_in_j].erase(
+                      neighbor_distss[i_in_j].begin() + k);
+                  break;
+                }
+              }
+            }
+          }
         }
       }
+    } // end while
+
+    size_t n_stable_particles = n_particles - n_rattlers;
+
+    size_t total_contacts = 0;
+    for (size_t i = 0; i < n_particles; ++i) {
+      total_contacts += neighbor_indss[i].size();
     }
+
+    int minimum_jammed_contacts = 2 * ((n_stable_particles - 1) * dim + 1);
+
+    if ((total_contacts < minimum_jammed_contacts) ||
+        (n_stable_particles < dim)) {
+      m_jammed = false;
+    }
+    m_jammed = true;
   }
 };
 
