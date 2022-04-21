34942a014f3b07192f4732471b9e7b749d28759f
Praharsh Suryadevara <praharsharmm@gmail.com>
2022-04-21 12:45:33 -0400

    miscellanous fixes

--- pele/potentials/__init__.py
+++ pele/potentials/__init__.py
@@ -65,6 +65,7 @@ from ._hs_wca_cpp import HS_WCA
 from ._inversepower_cpp import InversePower
 from ._inversepower_stillinger_cpp import InversePowerStillinger
 from ._inversepower_stillinger_cut_cpp import InversePowerStillingerCut
+from .combine_potentials import CombinedPotential
 from ._wca_cpp import *
 from ._harmonic_cpp import Harmonic
 from ._sumgaussianpot_cpp import SumGaussianPot
