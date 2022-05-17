0c604aae9f074378c5880e32bba76ab648145858
Praharsh Suryadevara <praharsharmm@gmail.com>
2022-05-17 10:37:16 -0400

    extended mixed descent

---
+++ source/extended_mixed_descent.cpp
@@ -0,0 +1,265 @@
+
+#include "pele/mxopt.hpp"
+#include "pele/array.hpp"
+#include "pele/base_potential.hpp"
+#include "pele/debug.hpp"
+#include "pele/eigen_interface.hpp"
+#include "pele/lbfgs.hpp"
+#include "pele/lsparameters.hpp"
+#include "pele/optimizer.hpp"
+#include <algorithm>
+#include <complex>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <sunlinsol/sunlinsol_dense.h> // access to dense SUNLinearSolver
+#include <sunlinsol/sunlinsol_spgmr.h> /* access to SPGMR SUNLinearSolver */
+#include <sunnonlinsol/sunnonlinsol_newton.h>
+
+using namespace Spectra;
+
+namespace pele {
+
+MixedOptimizer::MixedOptimizer(std::shared_ptr<pele::BasePotential> potential,
+                               const pele::Array<double> x0, double tol, int T,
+                               double step, double conv_tol, double conv_factor,
+                               double rtol, double atol, bool iterative)
+    : GradientOptimizer(potential, x0, tol), cvode_mem(CVodeCreate(CV_BDF)),
+      N_size(x_.size()), t0(0), tN(100.0), rtol(rtol), atol(atol),
+      xold(x_.size()), gold(x_.size()), step(x_.size()), T_(T), usephase1(true),
+      conv_tol_(conv_tol), conv_factor_(conv_factor), n_phase_1_steps(0),
+      n_phase_2_steps(0), hessian(x_.size(), x_.size()),
+      hessian_shifted(x_.size(), x_.size()), line_search_method(this, step) {
+
+  // assume previous phase is phase 1
+  prev_phase_is_phase1 = true;
+
+  // set precision of printing
+  // dummy t0
+  double t0 = 0;
+  uplo = 'U';
+
+  Array<double> x0copy = x0.copy();
+  x0_N = N_Vector_eq_pele(x0copy);
+  // initialization of everything CVODE needs
+  int ret = CVodeInit(cvode_mem, f, t0, x0_N);
+  // initialize userdata
+  udata.rtol = rtol;
+  udata.atol = atol;
+  udata.nfev = 0;
+  udata.nhev = 0;
+  udata.pot_ = potential_;
+  udata.stored_grad = Array<double>(x0.size(), 0);
+  // set tolerances
+  CVodeSStolerances(cvode_mem, udata.rtol, udata.atol);
+  ret = CVodeSetUserData(cvode_mem, &udata);
+  // initialize hessian
+  if (iterative) {
+    LS = SUNLinSol_SPGMR(x0_N, PREC_NONE, 0);
+    CVodeSetLinearSolver(cvode_mem, LS, NULL);
+  } else {
+    A = SUNDenseMatrix(N_size, N_size);
+    LS = SUNLinSol_Dense(x0_N, A);
+    CVodeSetLinearSolver(cvode_mem, LS, A);
+    CVodeSetJacFn(cvode_mem, Jac);
+  }
+
+  // pass hessian information
+  g_ = udata.stored_grad;
+  // initialize CVODE steps and stop time
+  CVodeSetMaxNumSteps(cvode_mem, 100000);
+  CVodeSetStopTime(cvode_mem, tN);
+  inv_sqrt_size = 1 / sqrt(x_.size());
+  // optmizeer debug level
+  std::cout << OPTIMIZER_DEBUG_LEVEL << "optimizer debug level \n";
+
+#if OPTIMIZER_DEBUG_LEVEL >= 1
+  std::cout << "Mixed optimizer constructed"
+            << "T=" << T_ << "\n";
+#endif
+}
+/**
+ * Does one iteration of the optimization algorithm
+ */
+void MixedOptimizer::one_iteration() {
+  if (!func_initialized_) {
+    initialize_func_gradient();
+  }
+
+  // declare that the hessian hasn't been calculated for this iteration
+  hessian_calculated = false;
+  // make a copy of the position and gradient
+  xold.assign(x_);
+  gold.assign(g_);
+  // copy the gradient into step
+
+  // does a convexity check every T iterations
+  // but always starts off in differential equation solver mode
+  if ((iter_number_ % T_ == 0 and iter_number_ > 0) or !usephase1) {
+#if OPTIMIZER_DEBUG_LEVEL >= 3
+    std::cout << "checking convexity"
+              << "\n";
+#endif
+    // check if landscape is convex
+    usephase1 = !convexity_check();
+    hessian_calculated = true;
+  }
+  if (usephase1) {
+#if OPTIMIZER_DEBUG_LEVEL >= 3
+    std::cout << " computing phase 1 step"
+              << "\n";
+#endif
+
+    compute_phase_1_step(step);
+  } else {
+
+#if OPTIMIZER_DEBUG_LEVEL >= 3
+    std::cout << " computing phase 2 step"
+              << "\n";
+#endif
+    step.assign(g_);
+    compute_phase_2_step(step);
+    line_search_method.set_xold_gold_(xold, gold);
+    line_search_method.set_g_f_ptr(g_);
+    double stepnorm = line_search_method.line_search(x_, step);
+    // switchtophase2 = true;
+  }
+
+  // should think of using line search method within the phase steps
+
+  // update inverse hessian estimate
+
+#if OPTIMIZER_DEBUG_LEVEL >= 3
+  std::cout << "mixed optimizer: " << iter_number_ << " E " << f_ << " rms "
+            << rms_ << " nfev " << nfev_ << " nhev " << udata.nhev << std::endl;
+#endif
+  /**
+   * Checks whether the stop criterion is satisfied: if stop criterion is
+   * satisfied The new version is done. at this point the corresponding values
+   * are added
+   */
+
+  iter_number_ += 1;
+}
+
+/**
+ * resets the minimizer for usage again
+ */
+void MixedOptimizer::reset(pele::Array<double> &x0) {
+  if (x0.size() != x_.size()) {
+    throw std::invalid_argument("The number of degrees of freedom (x0.size()) "
+                                "cannot change when calling reset()");
+  }
+  iter_number_ = 0;
+  nfev_ = 0;
+  x_.assign(x0);
+  initialize_func_gradient();
+}
+
+/**
+ * checks convexity in the region and updates the convexity flag accordingly
+ * convexity flag 0/False -> Not near the minimum: Use an differential equation
+ * solver (CVODE_BDF in this example). convexity flag true -> concavity below a
+ * tolerance, we think we're near a minimum. can be solved with newton steps
+with help convexity flag false ->
+ * convex function, We think we're near a minimum, use a newton method to figure
+out the result.
+ */
+bool MixedOptimizer::convexity_check() {
+
+  get_hess(hessian);
+
+  hessian_shifted = hessian;
+  // shift diagonal by conv tol
+
+  hessian_shifted.diagonal().array() += conv_tol_;
+
+  // check if the hessian is positive definite by doing a cholesky decomposition
+  // and checking if it is successful
+
+  // Eigen::ComputationInfo info = hessian_shifted.llt().info();
+  // std::cout << info << " success info of cholesky decomposition \n";
+
+  hess_shifted_data = hessian_shifted.data();
+
+  int N_int = x_.size();
+  dpotrf_(&uplo, &N_int, hess_shifted_data, &N_int, &info);
+  if (info == 0) {
+    // if it is positive definite, we can use the newton method to solve the
+    // problem
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * Gets the hessian. involves a dense hessian for now. #TODO replace with a
+ * sparse hessian. TODO: Allocate memory once
+ */
+void MixedOptimizer::get_hess(Eigen::MatrixXd &hessian) {
+  Array<double> hessian_pele = Array<double>(hessian.data(), hessian.size());
+  potential_->get_hessian(
+      x_, hessian_pele); // preferably switch this to sparse Eigen
+  udata.nhev += 1;
+}
+
+// /**
+//  * Phase 1 The problem does not look convex, Try solving using with an
+//  adaptive differential equationish approach
+//  */
+// void MixedOptimizer::compute_phase_1_step(Array<double> step) {
+//     // use a a scaled steepest descent step
+//     step *= -std::abs(H0_);
+// }
+
+/**
+ * Phase 1 The problem does not look convex, Try solving using with sundials
+ */
+void MixedOptimizer::compute_phase_1_step(Array<double> step) {
+  /* advance solver just one internal step */
+  Array<double> xold = x_;
+  // use this variable to compute differences and add to nhev later
+  double udatadiff = udata.nfev;
+  int flag = CVode(cvode_mem, tN, x0_N, &t0, CV_ONE_STEP);
+  udatadiff = udata.nfev - udatadiff;
+  nfev_ += udatadiff;
+  x_ = pele_eq_N_Vector(x0_N);
+  // here we have to translate from ODE solver language to optimizer language.
+  // this is because CVODE calculates the negative of the gradient.
+  g_ = -udata.stored_grad;
+  rms_ = (norm(g_) / sqrt(x_.size()));
+  f_ = udata.stored_energy;
+  step = xold - x_;
+  n_phase_1_steps += 1;
+  prev_phase_is_phase1 = true;
+}
+
+/**
+ * Phase 2 The problem looks convex enough to switch to a newton method
+ */
+void MixedOptimizer::compute_phase_2_step(Array<double> step) {
+
+  if (hessian_calculated == false) {
+    // we can afford to perform convexity checks at every steps
+    // assuming the rate limiting cost is the hessian which is calculated
+    // in the convexity check anyway
+    convexity_check();
+  }
+
+  hessian.diagonal().array() += conv_factor_ * conv_tol_ * 10;
+
+  Eigen::VectorXd r(step.size());
+  Eigen::VectorXd q(step.size());
+
+  q.setZero();
+  eig_eq_pele(r, step);
+
+  // negative sign to switch direction
+  // TODO change this to banded
+  q = -hessian.householderQr().solve(r);
+  pele_eq_eig(step, q);
+  n_phase_2_steps += 1;
+  prev_phase_is_phase1 = false;
+}
+} // namespace pele
---
+++ source/pele/extended_mixed_descent.hpp
@@ -0,0 +1,183 @@
+#ifndef _PELE_MIXED_OPT_H__
+#define _PELE_MIXED_OPT_H__
+
+#include "array.hpp"
+#include "base_potential.hpp"
+#include "debug.hpp"
+
+
+// #define EIGEN_USE_MKL_ALL
+// Eigen linear algebra library
+#include "eigen_interface.hpp"
+#include "pele/lbfgs.hpp"
+#include <Eigen/Dense>
+#include <Spectra/SymEigsSolver.h>
+#include <memory>
+
+// Lapack for cholesky
+extern "C" {
+#include <lapacke.h>
+}
+
+// line search methods
+#include "backtracking.hpp"
+#include "bracketing.hpp"
+#include "cvode.hpp"
+#include "linesearch.hpp"
+#include "more_thuente.hpp"
+#include "nwpele.hpp"
+#include "optimizer.hpp"
+
+#include <cvode/cvode.h> /* access to CVODE                 */
+#include <nvector/nvector_serial.h> /* access to serial N_Vector       */
+#include <sunlinsol/sunlinsol_dense.h> /* access to dense SUNLinearSolver */
+#include <sunmatrix/sunmatrix_dense.h> /* access to dense SUNMatrix       */
+
+extern "C" {
+#include "xsum.h"
+}
+
+namespace pele {
+
+// /**
+//  * user data passed to CVODE
+//  */
+// typedef struct UserData_
+// {
+
+//     double rtol; /* integration tolerances */
+//     double atol;
+//     size_t nfev;                // number of gradient(function) evaluations
+//     size_t nhev;                // number of hessian (jacobian) evaluations
+//     double stored_energy = 0;       // stored energy
+//     Array<double>    stored_grad;      // stored gradient. need to pass this
+//     on to std::shared_ptr<pele::BasePotential> pot_;
+// } * UserData;
+
+/**
+ * Mixed optimization scheme that uses different methods of solving far and away
+ * from the minimum/ somewhat close to the minimum and near the minimum
+ */
+class MixedOptimizer : public GradientOptimizer {
+private:
+  /**
+   * H0 is the initial estimate for the inverse hessian. This is as small as
+   * possible, for making a good inverse hessian estimate next time.
+   */
+  /**
+   * User_Data object. This is important for mxopt because it stores the number
+   * of getEnergyGradient and getEnegyGradientHessian evaluations. We're saving
+   * them here instead of the optimizer because we'd need to keep a double count
+   * since SUNDIALS needs this object.
+   */
+  UserData_ udata;
+  void *cvode_mem; /* CVODE memory         */
+  size_t N_size;
+  SUNMatrix A;
+  SUNLinearSolver LS;
+  double t0;
+  double tN;
+  N_Vector x0_N;
+  double rtol;
+  double atol;
+
+  Array<double> xold; //!< Coordinates before taking a step
+  Array<double> gold; //!< Gradient before taking a step
+  Array<double> step; //!< Step size and direction
+  double
+      inv_sqrt_size; //!< The inverse square root the the number of components
+  // Preconditioning
+  int T_; // number of steps after which the lowest eigenvalues are recalculated
+          // in the first phase
+  // std::shared_ptr<Eigen::ColPivHouseholderQR<Eigen::MatrixXd>> solver;
+  // solver for H x = b
+  Eigen::VectorXd
+  update_solver(Eigen::VectorXd r); // updates the solver with the new hessian
+
+  char uplo; /* We ask LAPACK for the lower diagonal matrix L */
+  int info;  /* "Info" return value, used for error-checking */
+
+  // Calculates hess + delta I where delta makes the new eigenvalue positive
+  Eigen::MatrixXd hessian;
+  Eigen::MatrixXd hessian_shifted;
+
+  double *hess_shifted_data;
+  bool usephase1;
+  // what phase was used in previous step
+  bool prev_phase_is_phase1;
+  /**
+   * number of phase 1 steps
+   */
+  size_t n_phase_1_steps;
+  /**
+   * number of phase 2 steps
+   */
+  size_t n_phase_2_steps;
+  /**
+   * tolerance for convexity. the smaller, the more convex the problem
+   * needs to be before switching to newton
+   */
+  double conv_tol_;
+  /**
+   * -conv_factor*\lambda_min is addeed to the hessian to make it
+   * covex if it is not convex
+   */
+  double conv_factor_;
+  // Need to refactor line searches
+  BacktrackingLineSearch line_search_method;
+
+public:
+  /**
+   * Constructor
+   */
+  MixedOptimizer(std::shared_ptr<pele::BasePotential> potential, std::shared_ptr<pele::BasePotential> potential_extension,
+                 const pele::Array<double> x0, double tol = 1e-4, int T = 1,
+                 double step = 1, double conv_tol = 1e-8,
+                 double conv_factor = 2, double rtol = 1e-5, double atol = 1e-5,
+                 bool iterative = false);
+  /**
+   * Destructor
+   */
+  virtual ~MixedOptimizer() {}
+
+
+  /**
+   * extension of the original potential that ensures the hessian is positive in the 
+   */
+  std::shared_ptr<pele::BasePotential> extended_potential;
+  /**
+   * Do one iteration iteration of the optimization algorithm
+   */
+  void one_iteration();
+
+  // functions for accessing the results
+
+  /**
+   * reset the optiimzer to start a new minimization from x0
+   */
+  virtual void reset(pele::Array<double> &x0);
+  inline int get_nhev() const { return udata.nhev; }
+  inline int get_n_phase_1_steps() { return n_phase_1_steps; }
+  inline int get_n_phase_2_steps() { return n_phase_2_steps; }
+
+private:
+  bool hessian_calculated; // checks whether the hessian has been calculated for
+                           // updating.
+  void compute_phase_1_step(Array<double> step);
+
+  void compute_phase_2_step(Array<double> step);
+  bool convexity_check();
+  bool minimum_less_than_zero;
+  void get_hess(Eigen::MatrixXd &hess);
+
+  void update_H0_(Array<double> x_old, Array<double> &g_old,
+                  Array<double> x_new, Array<double> &g_new);
+  // Does normal LBFGS without preconditioning
+  double hessnorm;
+  double minimum;
+  double pf;
+};
+
+} // namespace pele
+
+#endif
