15e0c592414b07a3320158acba3c18edcca39fcf
Praharsh Suryadevara <praharsharmm@gmail.com>
2022-06-09 14:45:27 -0400

    convex region checks

--- source/mixed_descent_with_fire.cpp
+++ source/mixed_descent_with_fire.cpp
@@ -9,7 +9,71 @@
  *
  */
 
-
 #include "pele/mixed_descent_with_fire.hpp"
+#include <lapack.h>
+#include <pele/array.hpp>
+
+namespace pele {
+bool GenericMixedDescent::x_in_convex_region() {
+
+  x_ = opt_non_convex_->get_x();
+  // Calculate hessian at x. Current version is hessian based.
+  // Ideally you should be able to get the pointer to the hessian from the optimizer.
+  get_hess(hessian_);
 
 
+  int N_int = x_.size();
+  add_translation_offset_2d(hessian_, translation_offset_);
+
+
+  hessian_copy_for_cholesky_ = hessian_;
+
+  // pointer to t
+  double * hessian_copy_for_cholesky_data;
+  hessian_copy_for_cholesky_data = hessian_copy_for_cholesky_.data();
+  int info = 0;
+
+  // 1 is the length of the uplo string, necessary for current fortran compilers.
+  dpotrf_(&uplo, &N_int, hessian_copy_for_cholesky_data, &N_int, &info, 1);
+
+  if (info == 0) {
+    // if positive definite, we are in the convex region.
+    return true;
+  }
+  return false;
+}
+
+
+void GenericMixedDescent::get_hess(Eigen::MatrixXd &hessian) {
+  // Does not allocate memory for hessian just wraps around the data
+  Array<double> hessian_pele = Array<double>(hessian.data(), hessian.size());
+  potential_->get_hessian(
+      x_, hessian_pele); // preferably switch this to sparse Eigen
+  hessian_evaluations_ += 1;
+}
+
+
+/**
+ * @brief  adds a translation offset to the hessian. This should take care of
+ * translational symmetries
+ * @param  hessian the hessian to be modified
+ * @param  offset the offset to be added
+ */
+void GenericMixedDescent::add_translation_offset_2d(Eigen::MatrixXd &hessian,
+                                                       double offset) {
+
+  // factor so that the added translation operators are unitary
+  double factor = 2.0 / hessian.rows();
+  offset = offset * factor;
+
+  for (size_t i = 0; i < hessian.rows(); ++i) {
+    for (size_t j = i + 1; j < hessian.cols(); ++j) {
+      if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
+        hessian(i, j) += offset;
+        hessian(j, i) += offset;
+      }
+    }
+  }
+  hessian.diagonal().array() += offset;
+}
+} // namespace pele
--- source/pele/mixed_descent_with_fire.hpp
+++ source/pele/mixed_descent_with_fire.hpp
@@ -12,46 +12,87 @@
  *
  */
 
-
 #ifndef PELE_MIXED_DESCENT_WITH_FIRE_HPP
 #define PELE_MIXED_DESCENT_WITH_FIRE_HPP
 
-#include <pele/optimizer.h>
+#include <Eigen/Dense>
+#include <Eigen/src/Core/Matrix.h>
+#include <cstddef>
+#include <memory>
+#include <pele/array.hpp>
+#include <pele/base_potential.hpp>
 #include <pele/optimizer.hpp>
-
 
 namespace pele {
 
-
 /**
- * @brief Generalized Mixed Descent that combines two optimizers
+ * @brief Generalized Mixed Descent that combines two optimizers.
  * @details Base class for combining two optimizers.
  */
-class GenericMixedDescent : public Optimizer {
+class GenericMixedDescent : public GradientOptimizer {
 public:
-    GenericMixedDescent(
-        std::shared_ptr<pele::Optimizer> opt_1,
-        std::shared_ptr<pele::Optimizer> opt_2,
-    ) :
-        opt_1_(opt_1),
-        opt_2_(opt_2)
-    {}
+  GenericMixedDescent(std::shared_ptr<BasePotential> pot, pele::Array<double> x,
+                      double tol,
+                      std::shared_ptr<pele::GradientOptimizer> opt_non_convex,
+                      std::shared_ptr<pele::GradientOptimizer> opt_convex,
+                      double translation_offset)
+      : opt_non_convex_(opt_non_convex), opt_convex_(opt_convex),
+        translation_offset_(translation_offset), hessian_(x.size(), x.size()),
+        hessian_copy_for_cholesky_(x.size(), x.size()), hessian_evaluations_(0),
+        iterations_(0), GradientOptimizer(pot, x, tol) {
+    // Ensure that both optimizers have the same potential
+    // There is redundancy because we add the potential to each optimizer
+    // but this overrides the potential in the optimizer.
+    opt_non_convex_->set_potential(pot);
+    opt_convex_->set_potential(pot);
+    opt_convex_->set_tolerance(tol);
+    opt_non_convex_->set_x(x);
+    opt_non_convex_->set_tolerance(tol);
+    uplo = 'U';
+  }
+
+  void one_iteration();
+
 private:
-    std::shared_ptr<pele::Optimizer> opt_1_;
-    std::shared_ptr<pele::Optimizer> opt_2_;
+  std::shared_ptr<pele::GradientOptimizer> opt_non_convex_;
+  std::shared_ptr<pele::GradientOptimizer> opt_convex_;
 
+  /**
+   * @brief criterion for switching from non-convex to convex optimizer.
+   * @details Checks whether the current position of Mixed Descent is in a
+   * convex region or not
+   *
+   */
+  bool x_in_convex_region();
 
+  /**
+   * @brief Calculate the hessian at the current position.
+   * @details Calculate the hessian at the current position.
+   *
+   * @param hessian the hessian to be calculated
+   */
+  void get_hess(Eigen::MatrixXd &hessian);
 
+  /**
+   * @brief Uses convex method to find the next point if true, else uses
+   * non-convex method.
+   */
+  bool use_convex_method_;
+
+  void add_translation_offset_2d(Eigen::MatrixXd &hessian, double offset);
+
+  size_t hessian_evaluations_;
+  size_t iterations_;
+
+  double translation_offset_;
+  Eigen::MatrixXd hessian_;
+
+  // Space allocated for a copy of the hessian, this will be
+  // rewritten with the Cholesky factorization of the hessian.
+  Eigen::MatrixXd hessian_copy_for_cholesky_;
+
+  char uplo;
 };
-}
-
-
-
-
-
-
-
+} // namespace pele
 
 #endif // !1
-
-
