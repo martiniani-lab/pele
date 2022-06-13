180049ea45f40426cac7c6f4d14205ccfe0830b5
Praharsh Suryadevara <praharsharmm@gmail.com>
2022-06-13 15:10:24 -0400

    fixes for compilation

--- pele/optimize/generic_mixed_descent.pyx
+++ pele/optimize/generic_mixed_descent.pyx
@@ -25,8 +25,9 @@ cimport cython
 cdef extern from "pele/generic_mixed_descent.hpp" namespace "pele":
     cdef cppclass cppGenericMixedDescent "pele::GenericMixedDescent":
         cppGenericMixedDescent(shared_ptr[_pele.cBasePotential], _pele.Array[double], 
-                               shared_ptr[_pele.cGradientOptimizer],
-                               shared_ptr[_pele.cGradientOptimizer],
+                               double tol,
+                               shared_ptr[_pele_opt.cGradientOptimizer],
+                               shared_ptr[_pele_opt.cGradientOptimizer],
                                double, int) except +
         double get_nhev() except +
         int get_n_phase_1_steps() except +
@@ -35,39 +36,40 @@ cdef extern from "pele/generic_mixed_descent.hpp" namespace "pele":
 
 
 
-cdef class _Cdef_MixedOptimizer_CPP(_pele_opt.GradientOptimizer):
-    """This class is the python interface for the c++ MixedOptimizer implementation
+cdef class _Cdef_GenericMixedDescent_CPP(_pele_opt.GradientOptimizer):
+    """This class is the python interface for the c++ GenericMixedDescent implementation
     """
     cdef _pele.BasePotential pot
-    cdef _pele.GradientOptimizer opt_conv
-    cdef _pele.GradientOptimizer opt_non_conv
-    def __cinit__(self, potential, x0,  optimizer_convex,
-                  optimizer_non_convex, double translation_offset,
+    cdef _pele_opt.GradientOptimizer opt_conv
+    cdef _pele_opt.GradientOptimizer opt_non_conv
+    def __cinit__(self, potential, x0, optimizer_convex,
+                  optimizer_non_convex, double tol, double translation_offset,
                   int steps_before_convex_check=1):
         
         potential = as_cpp_potential(potential, verbose=True)
         self.pot = potential
-        opt_conv = optimizer_convex
-        opt_non_conv = optimizer_non_convex
+        self.opt_conv = optimizer_convex
+        self.opt_non_conv = optimizer_non_convex
         cdef np.ndarray[double, ndim=1] x0c = np.array(x0, dtype=float)
         self.thisptr = shared_ptr[_pele_opt.cGradientOptimizer]( <_pele_opt.cGradientOptimizer*>
-                new cppMixedOptimizer(self.pot.thisptr,
+                new cppGenericMixedDescent(self.pot.thisptr,
                              _pele.Array[double](<double*> x0c.data, x0c.size), 
-                             opt_conv.thisptr,
-                             opt_non_conv.thisptr,
-                             offset,
+                             tol,
+                             self.opt_conv.thisptr,
+                             self.opt_non_conv.thisptr,
+                             translation_offset,
                              steps_before_convex_check
                              ))
-        cdef cppMixedOptimizer* mxopt_ptr = <cppMixedOptimizer*> self.thisptr.get()
+        cdef cppGenericMixedDescent* mxopt_ptr = <cppGenericMixedDescent*> self.thisptr.get()
 
     def get_result(self):
-        cdef cppMixedOptimizer* mxopt_ptr = <cppMixedOptimizer*> self.thisptr.get()
-        res = super(_Cdef_MixedOptimizer_CPP, self).get_result()
+        cdef cppGenericMixedDescent* mxopt_ptr = <cppGenericMixedDescent*> self.thisptr.get()
+        res = super(_Cdef_GenericMixedDescent_CPP, self).get_result()
         res["nhev"] = float(mxopt_ptr.get_nhev())
         res["n_phase_1"] = int(mxopt_ptr.get_n_phase_1_steps())
         res["n_phase_2"] = int(mxopt_ptr.get_n_phase_2_steps())
         return res
 
-class MixedOptimizer_CPP(_Cdef_MixedOptimizer_CPP):
+class GenericMixedDescent_CPP(_Cdef_GenericMixedDescent_CPP):
     """This class is the python interface for the c++ LBFGS implementation
     """
