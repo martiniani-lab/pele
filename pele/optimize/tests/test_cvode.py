7f129129222844b4c44efe954abaafd0d00ce9f6
Praharsh Suryadevara <praharsharmm@gmail.com>
2022-07-26 10:41:23 -0400

    add iterative test case

--- pele/optimize/tests/test_cvode.py
+++ pele/optimize/tests/test_cvode.py
@@ -150,4 +150,28 @@ def test_cvode_compare_with_cpp(potential_initial_and_final_conditions):
     return True
 
 
+def test_iterative_works_cvode(potential_initial_and_final_conditions):
+    """
+    Test the CVODE results through the python wrapper match with the C++ results.
+    """
+    potential, initial_coordinates, expected_final_coordinates = potential_initial_and_final_conditions
+    cvode_optimizer = CVODEBDFOptimizer(
+        potential,
+        initial_coordinates,
+        rtol=1e-10,
+        atol=1e-10,
+        tol=1e-9,
+        iterative=True,
+        use_newton_stop_criterion=True,
+    )
+    res = cvode_optimizer.run(10000)
+    print(res)
+    final_coords = res.coords
+
+    # check whether right minima is reached
+    assert np.allclose(final_coords, expected_final_coordinates)
+    # Expect steps to match with C++ results from test_cvode.cpp
+    return True
+
+
 
