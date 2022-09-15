c35ce36d0f174dd16727468c9ea2a7dc2845433e
Praharsh Suryadevara <praharsharmm@gmail.com>
2022-09-15 14:39:13 -0400

    add tests for extended mixed descent

--- pele/optimize/tests/test_extended_mixed_descent.py
+++ pele/optimize/tests/test_extended_mixed_descent.py
@@ -3,30 +3,36 @@ Tests for Extended Mixed Descent.
 #TODO: Extended Mixed Descent needs to be folded into Generic Mixed Descent.
 """
 
-from .potential_fixture import potential_initial_and_final_conditions, potential_extension
+from .potential_fixture import potential_initial_and_final_conditions
 from pele.optimize import ExtendedMixedOptimizer
 import numpy as np
+from pele.potentials import InversePower
 
 
 
 
-def test_extended_mixed_descent_compare_with_cpp(potential_initial_and_final_conditions, potential_extension):
+def test_extended_mixed_descent_compare_with_cpp(potential_initial_and_final_conditions):
     """
     Test the CVODE results through the python wrapper match with the C++ results.
     """
     
-    potential, initial_coordinates, expected_final_coordinates, _ = potential_initial_and_final_conditions
+    potential, initial_coordinates, expected_final_coordinates, pot_dict = potential_initial_and_final_conditions
+    
+    potential_extension = InversePower(**pot_dict)
+    
+    
     emd = ExtendedMixedOptimizer(
         potential,
         initial_coordinates,
         potential_extension,
-        rtol=1e-5,
-        atol=1e-5,
+        rtol=1e-6,
+        atol=1e-6,
         tol=1e-9,
         iterative=False,
     )
     res = emd.run(10000)
     final_coords = res.coords
+    print(res.nsteps)
     # check whether right minima is reached
     assert np.allclose(final_coords, expected_final_coordinates)
     # Expect steps to match with C++ results from test_cvode.cpp
@@ -49,6 +55,7 @@ def test_extended_mixed_descent_compare_with_cpp_iterative(potential_initial_and
         iterative=True,
     )
     res = emd.run(10000)
+    print(res.nsteps)
     final_coords = res.coords
     # check whether right minima is reached
     assert np.allclose(final_coords, expected_final_coordinates)
