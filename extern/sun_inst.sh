75735137c8386581b351d692cbac45123fbff50a
Praharsh Suryadevara <praharsharmm@gmail.com>
2022-10-12 14:00:07 -0400

    add external install script

---
+++ extern/sun_inst.sh
@@ -0,0 +1,25 @@
+# Script to install sundials for pele
+# first argument is the build type (debug or release)
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+c_d=$SCRIPT_DIR/install
+
+cd sundials
+mkdir build
+cd build
+
+
+# note the newlines are important
+
+cmake -DCMAKE_INSTALL_PREFIX=$c_d \
+      -DCMAKE_BUILD_TYPE=$1 \
+      -DENABLE_OPENMP=OFF  \
+      -DBUILD_STATIC_LIBS=OFF \
+      -DBUILD_SHARED_LIBS=ON \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+      -DCMAKE_CXX_STANDARD=17 \
+      -DSUNDIALS_INDEX_SIZE=32 \
+      ..
+make install
+cd ..
+rm -rf build
