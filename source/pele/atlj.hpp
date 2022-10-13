98a8dd2e506c9e5a8b3f4548cd66609009279bef
Praharsh Suryadevara <praharsharmm@gmail.com>
2022-10-12 20:08:21 -0400

    remove autodiff dependency

--- source/pele/atlj.hpp
+++ source/pele/atlj.hpp
@@ -20,9 +20,6 @@ extern "C" {
 }
 #include <Eigen/Core>
 
-#include <autodiff/reverse.hpp>
-#include <autodiff/reverse/eigen.hpp>
-
 using namespace autodiff;
 // using namespace Eigen;
 
@@ -36,36 +33,12 @@ private:
   double m_ndim;
   double m_natoms;
   LJ ljpot;
-  double get_AT_energy(Array<double> const &x);
-  double
-  get_AT_energy_gradient(Array<double> const &x,
-                         std::vector<xsum_small_accumulator> &exact_grad);
-  // partial derivative vector of r12**n*r23**n*r31**n useful for calculating
-  // gradient
-  void prod_derivative(double rij, double rjk, double rki, Array<double> drij,
-                       Array<double> drjk, Array<double> drki, double n,
-                       double prefactor,
-                       std::vector<xsum_small_accumulator> &exact_grad);
-  // partial derivative of product of piecewise dot products: useful for
-  // calculating gradient
-  void piecewise_derivative(double rij, double rjk, double rki,
-                            Array<double> drij, Array<double> drjk,
-                            Array<double> drki, double prefactor,
-                            std::vector<xsum_small_accumulator> &exact_grad);
-  var Atenergy(const Eigen::VectorXvar &x);
-  double get_AT_energy_gradient_hessian(Array<double> const &x,
-                                        Array<double> &grad,
-                                        Array<double> &hess);
+  double get_axilrod_teller_energy(Array<double> const &x);
 
 public:
   ATLJ(double sig, double eps, double Z);
   virtual ~ATLJ() {}
   double get_energy(Array<double> const &x);
-  double get_energy_gradient(Array<double> const &x,
-                             std::vector<xsum_small_accumulator> &exact_grad);
-  double get_energy_gradient(Array<double> const &x, Array<double> &grad);
-  double get_energy_gradient_hessian(Array<double> const &x,
-                                     Array<double> &grad, Array<double> &hess);
 };
 } // namespace pele
 
