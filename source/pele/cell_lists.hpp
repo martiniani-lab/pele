2481aa6718743740969016aebcc5827242d435a9
Praharsh Suryadevara <praharsharmm@gmail.com>
2022-12-29 11:59:02 -0500

    format ASSERT_EX

--- source/pele/cell_lists.hpp
+++ source/pele/cell_lists.hpp
@@ -18,13 +18,14 @@
 #include "queue.hpp"
 #include "vecn.hpp"
 
-
-
 #ifndef NDEBUG
-#define ASSERT_EX(condition, statement) \
-    do { \
-        if (!(condition)) { statement; assert(condition); } \
-    } while (false)
+#define ASSERT_EX(condition, statement)                                        \
+  do {                                                                         \
+    if (!(condition)) {                                                        \
+      statement;                                                               \
+      assert(condition);                                                       \
+    }                                                                          \
+  } while (false)
 #else
 #define ASSERT_EX(condition, statement) ((void)0)
 #endif
