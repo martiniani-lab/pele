6be77accf3902234317925874af01f3756dc7613
Praharsh Suryadevara <praharsharmm@gmail.com>
2023-01-24 20:25:54 -0500

    energy change tests for cell lists and refactoring

--- cpp_tests/source/test_energy_change.cpp
+++ cpp_tests/source/test_energy_change.cpp
@@ -1,70 +1,113 @@
 #include <cstddef>
 #include <gtest/gtest.h>
 #include <iostream>
+#include <memory>
+#include <vector>
 
 #include "pele/array.hpp"
+#include "pele/base_potential.hpp"
 #include "pele/inversepower.hpp"
 #include "pele/lbfgs.hpp"
+#include "pele/pairwise_potential_interface.hpp"
 
 using namespace pele;
 
 class EnergyChangeSimplePairwise : public ::testing::Test {
  public:
-  std::shared_ptr<BasePotential> simple_pairwise_inverse_power;
+  std::shared_ptr<PairwisePotentialInterface> potential;
   Array<double> quenched_coordinates;
   static constexpr size_t dim = 2;
+
+  std::vector<size_t> neighboring_indices;
+
+ protected:
+  static constexpr int pow = 2;
+  double eps;
+  double non_additivity;
+  bool exact_sum;
+  Array<double> boxvec;
+  Array<double> radii;
+
   void SetUp() {
-    constexpr int pow = 2;
-
     size_t n_particles = 32;
-    double eps = 1.0;
-    double non_additivity = 0.2;
-    bool exact_sum = false;
-
-    double cutoff_factor = 1.25;
+    eps = 1.0;
+    non_additivity = 0.2;
+    exact_sum = false;
 
     double dmin_by_dmax = 0.449;
     double d_mean = 1.0;
+    double phi = 1.2;
+
     BerthierDistribution3d berthier_dist =
         BerthierDistribution3d(dmin_by_dmax, d_mean);
-    Array<double> radii = berthier_dist.sample(n_particles);
+    radii = berthier_dist.sample(n_particles);
 
-    double phi = 1.2;
     double box_length = get_box_length(radii, dim, phi);
-    Array<double> boxvec = {box_length, box_length};
-    double ncellsx_scale = 1.0;
+    boxvec = {box_length, box_length};
 
     Array<double> coordinates =
         generate_random_coordinates(box_length, n_particles, dim);
 
-    simple_pairwise_inverse_power =
-        std::make_shared<InverseIntPowerPeriodic<dim, pow>>(
-            eps, radii, boxvec, exact_sum, non_additivity);
+    potential = std::make_shared<InverseIntPowerPeriodic<dim, pow>>(
+        eps, radii, boxvec, exact_sum, non_additivity);
 
-    LBFGS lbfgs = LBFGS(simple_pairwise_inverse_power, coordinates);
+    std::shared_ptr<BasePotential> potential_base =
+        static_pointer_cast<BasePotential>(potential);
+    LBFGS lbfgs = LBFGS(potential_base, coordinates);
     lbfgs.run();
     quenched_coordinates = lbfgs.get_x().copy();
+
+    Array<std::vector<size_t>> neighbor_indices_cache;
+    Array<std::vector<std::vector<double>>> neighbor_distances_cache;
+    potential->get_neighbors(quenched_coordinates, neighbor_indices_cache,
+                             neighbor_distances_cache);
+
+    // Search indices for two particles that are neighbors
+    bool found;
+    for (std::size_t i = 0; i < n_particles; ++i) {
+      // Automatically takes care of case when particle has no neighbors
+      // (i.e. neighbor_indices_cache[i].size() == 0)
+      for (std::size_t j = 0; j < neighbor_indices_cache[i].size(); ++j) {
+        neighboring_indices = {i, neighbor_indices_cache[i][j]};
+        return;
+      }
+    }
+  }
+
+  void test_energy_change() {
+    double old_energy = potential->get_energy(quenched_coordinates);
+    double delta = 0.1;
+
+    // Calculate whether the change is correctly calculated
+    Array<double> new_coordinates = quenched_coordinates.copy();
+
+    // Change them in opposite directions
+    // to make sure there is an energy change
+    // due to the two particles themselves
+    new_coordinates[neighboring_indices[0] * dim] += delta;
+    new_coordinates[neighboring_indices[1] * dim] -= delta;
+
+    // Changed ind
+    double new_energy = potential->get_energy(new_coordinates);
+
+    double energy_change = potential->get_energy_change(
+        quenched_coordinates, new_coordinates, neighboring_indices);
+    ASSERT_NEAR(new_energy, old_energy + energy_change, 1e-9);
   }
 };
 
-TEST_F(EnergyChangeSimplePairwise, TestEnergyChange) {
-  double old_energy =
-      simple_pairwise_inverse_power->get_energy(quenched_coordinates);
-
-  double delta = 0.1;
-
-  // Calculate whether the change is correctly calculated
-  std::vector<size_t> changed_indices = {2, 5};
-  Array<double> new_coordinates = quenched_coordinates.copy();
-
-  //
-  for (size_t i : changed_indices) {
-    new_coordinates[i * dim] += delta;
+class EnergyChangeCellLists : public EnergyChangeSimplePairwise {
+ private:
+ public:
+  void SetUp() {
+    double ncellx_scale = 1.0;
+    EnergyChangeSimplePairwise::SetUp();
+    // Make the same potential but with cell lists
+    potential = std::make_shared<InverseIntPowerPeriodicCellLists<dim, pow>>(
+        A eps, radii, boxvec, ncellx_scale, exact_sum, non_additivity);
   }
-  double new_energy =
-      simple_pairwise_inverse_power->get_energy(new_coordinates);
+};
 
-  double energy_change = simple_pairwise_inverse_power->get_energy_change(
-      quenched_coordinates, new_coordinates, changed_indices);
-  ASSERT_NEAR(new_energy, old_energy + energy_change, 1e-9);
-}
+TEST_F(EnergyChangeSimplePairwise, TestEnergyChange) { test_energy_change(); }
+
+TEST_F(EnergyChangeCellLists, TestEnergyChange) { test_energy_change(); }
