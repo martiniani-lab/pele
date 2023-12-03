ec3f4db5a6504e5f1138e001120f86add6cd1b9f
Praharsh Suryadevara <praharsharmm@gmail.com>
2023-12-03 00:06:12 -0500

    change CosProduct function to PoweredCosineSum

--- pele/potentials/cpp_test_functions.pyx
+++ pele/potentials/cpp_test_functions.pyx
@@ -13,27 +13,32 @@ from pele.potentials._pele cimport array_wrap_np, array_wrap_np_size_t
 
 
 cdef extern from "pele/rosenbrock.hpp" namespace "pele":
-    cdef cppclass cNegativeCosProduct "pele::NegativeCosProduct":
-        cNegativeCosProduct(size_t dim, double period) except +
+    cdef cppclass cPoweredCosineSum "pele::PoweredCosineSum":
+        cPoweredCosineSum(size_t dim, double period, double cpower, double coffset) except +
         
         
         
-cdef class _Cdef_NegativeCosProduct(_pele.BasePotential):
-    """Define the python interface to the c++ NegativeCosProduct potential implementation
+cdef class _Cdef_PoweredCosineSum(_pele.BasePotential):
+    """Define the python interface to the c++ PoweredCosineSum potential implementation
     """
     cdef int cdim
     cdef double cperiod
+    cdef double cpower
+    cdef double coffset
     
     
-    def __cinit__(self, int dim, double period):
+    
+    def __cinit__(self, int dim, double period, double power=0.5, double offset = 0.0):
         self.cdim = dim
         self.cperiod = period
-        self.thisptr = shared_ptr[_pele.cBasePotential](<_pele.cBasePotential*>new cNegativeCosProduct(self.cdim, self.cperiod))
+        self.cpower = power
+        self.coffset = offset
+        self.thisptr = shared_ptr[_pele.cBasePotential](<_pele.cBasePotential*>new cPoweredCosineSum(self.cdim, self.cperiod, self.cpower, self.coffset))
     
     def __reduce__(self):
-        return (NegativeCosProduct, (self.cdim, self.cperiod))
+        return (PoweredCosineSum, (self.cdim, self.cperiod, self.cpower, self.coffset))
     
     
-class NegativeCosProduct(_Cdef_NegativeCosProduct):
-    """Python Interface to the c++ NegativeCosProduct potential implementation
+class PoweredCosineSum(_Cdef_PoweredCosineSum):
+    """Python Interface to the c++ PoweredCosineSum potential implementation
     """
